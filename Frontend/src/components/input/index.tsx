fcd4cd9db08879bf327ea5248db28eae1312d44a
HScokies <hscokies@outlook.com>
2023-12-05 21:28:30 +0500

    password show button

--- Frontend/src/components/input/index.tsx
+++ Frontend/src/components/input/index.tsx
@@ -12,11 +12,13 @@ interface props {
     maxlength?: number
 }
 
-const Input = ({ label, type, name, required = false, validator, onChange, defaultValue, maxlength }: props) => {
+const Input = ({ label, type, name, required = false, validator, onChange, defaultValue = "", maxlength }: props) => {
 
-    const [error, setError] = useState<string | undefined>()
+    const [error, setError] = useState<string>()
     const [active, setActive] = useState(defaultValue ? true : false)
     const [value, setValue] = useState(defaultValue)
+
+    const [showPassword, setShowPassword] = useState(false)
 
     const EnableField = async () => {
         await setActive(true)
@@ -40,8 +42,16 @@ const Input = ({ label, type, name, required = false, validator, onChange, defau
     return (
         <div className="input">
             <div className={`input_wrapper ${error ? 'error' : ''}`} onClick={() => EnableField()}>
-                <input maxLength={maxlength} value={value} onChange={(e) => { if (onChange) onChange(e); setValue(e.target.value) }} onFocus={() => EnableField()} onBlur={() => handleBlur()} className={`input_wrapper-field${active ? ' active' : ''}`} id={name} name={name} type={type} />
+                <input maxLength={maxlength} value={value} onChange={(e) => { if (onChange) onChange(e); setValue(e.target.value) }} onFocus={() => EnableField()} onBlur={() => handleBlur()} className={`input_wrapper-field${active ? ' active' : ''}`} id={name} name={name} type={showPassword ? "text" : type} />
                 <label className={`input_wrapper-label${active ? ' active' : ''}`} htmlFor={name}>{label}</label>
+                {
+                    type == "password" && value?.length > 0 ?
+                        showPassword ?
+                            <svg onClick={() => setShowPassword(false)} className='input_wrapper-show' focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78l3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z" ></path></svg>
+                            :
+                            <svg onClick={() => setShowPassword(true)} className='input_wrapper-show' focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"></path></svg>
+                        : null
+                }
             </div>
 
             <p className="input-error">

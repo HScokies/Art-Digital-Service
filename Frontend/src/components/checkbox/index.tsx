90169233a50c1669cfe8950ae54aaa5b3bb180ea
HScokies <hscokies@outlook.com>
2024-02-26 20:08:18 +0500

    Lowered logo.webp resolution + fixed checkbox bug

--- Frontend/src/components/checkbox/index.tsx
+++ Frontend/src/components/checkbox/index.tsx
@@ -6,31 +6,28 @@ interface props {
     children: any,
     name: string,
     defaultValue?: boolean,
-    checkedChanged?: ()=>void
+    checkedChanged?: () => void
 }
 
-const Checkbox = ({ children, name, defaultValue = false, checkedChanged }: props) => {
+const Checkbox = ({ children, name, defaultValue, checkedChanged }: props) => {
     const [checked, setChecked] = useState(defaultValue)
     useEffect(() => {
         if (checkedChanged)
             checkedChanged()
-    },[checked])
+    }, [checked])
 
     return (
-        <div className='checkbox' onClick={() => {if (checkedChanged) checkedChanged()}}>
+        <div className='checkbox' onClick={() => setChecked(!checked)}>
             <label className='checkbox-label' htmlFor={name}>
-                {
-                    children
-                }
+                {children}
             </label>
-            <input onChange={() => setChecked(!checked)} className='checkbox-input' type='checkbox' id={name} name={name} checked={checked} />
-            <span onClick={() => setChecked(!checked)} className={'checkbox-input_box'}>
-                {
-                    checked ?
-                        <svg>
-                            <use xlinkHref={Icons+'#check'} href={Icons+'#check'} />
-                        </svg> :
-                        null
+            <input className='checkbox-input' type='checkbox' id={name} name={name} onChange={(e) => e.stopPropagation()} checked={checked} onClick={(e) => e.stopPropagation()} />
+            <span className={'checkbox-input_box'} >
+                {checked ?
+                    <svg>
+                        <use xlinkHref={Icons + '#check'} href={Icons + '#check'} />
+                    </svg> :
+                    null
                 }
             </span>
         </div>
